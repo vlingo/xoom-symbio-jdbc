12189bd56ac70e4494e459a87554674f37e0dfbb
Kevin Mas Ruiz <masruizkevin@gmail.com>
2018-10-28 19:50:00 +0100

    Add event journal

---
+++ src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/eventjournal/PostgresEventJournal.java
@@ -0,0 +1,76 @@
+package io.vlingo.symbio.store.state.jdbc.postgres.eventjournal;
+
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.Address;
+import io.vlingo.actors.Definition;
+import io.vlingo.common.Completes;
+import io.vlingo.symbio.Event;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.store.eventjournal.EventJournal;
+import io.vlingo.symbio.store.eventjournal.EventJournalReader;
+import io.vlingo.symbio.store.eventjournal.EventStreamReader;
+import io.vlingo.symbio.store.state.jdbc.Configuration;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class PostgresEventJournal<T> extends Actor implements EventJournal<T> {
+    private final Configuration configuration;
+    private final Connection connection;
+
+    public PostgresEventJournal(Configuration configuration, Connection connection) {
+        this.configuration = configuration;
+        this.connection = connection;
+    }
+
+    @Override
+    public void append(String streamName, int streamVersion, Event<T> event) {
+
+    }
+
+    @Override
+    public void appendWith(String streamName, int streamVersion, Event<T> event, State<T> snapshot) {
+
+    }
+
+    @Override
+    public void appendAll(String streamName, int fromStreamVersion, List<Event<T>> events) {
+
+    }
+
+    @Override
+    public void appendAllWith(String streamName, int fromStreamVersion, List<Event<T>> events, State<T> snapshot) {
+
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Completes<EventJournalReader<T>> eventJournalReader(String name) {
+        Address address = stage().world().addressFactory().from("eventJournalReader-" + name);
+        EventJournalReader<T>  reader = stage().actorFor(
+                Definition.has(
+                        PostgresEventJournalReader.class,
+                        Definition.parameters(configuration, name)
+                ),
+                EventJournalReader.class,
+                address
+        );
+
+        return completes().with(reader);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Completes<EventStreamReader<T>> eventStreamReader(String name) {
+        Address address = stage().world().addressFactory().from("eventStreamReader-" + name);
+        EventStreamReader<T> reader = stage().actorFor(
+                Definition.has(
+                        PostgresEventStreamReader.class,
+                        Definition.parameters(configuration)),
+                EventStreamReader.class,
+                address
+        );
+
+        return completes().with(reader);
+    }
+}
