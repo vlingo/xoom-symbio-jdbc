af5ab6d5f5925294efac0d0c66ef8956026f3e13
Vaughn Vernon <vaughn@forcomprehension.com>
2018-10-29 21:19:49 -0700

    Implemented AppendResultInterest support.

--- src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/eventjournal/PostgresEventJournalReaderActor.java
+++ src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/eventjournal/PostgresEventJournalReaderActor.java
@@ -1,3 +1,10 @@
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
 package io.vlingo.symbio.store.state.jdbc.postgres.eventjournal;
 
 import com.google.gson.Gson;
@@ -67,7 +74,6 @@ public class PostgresEventJournalReaderActor extends Actor implements EventJourn
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public Completes<Event<String>> readNext() {
         try {
             querySingleEvent.setLong(1, offset);
