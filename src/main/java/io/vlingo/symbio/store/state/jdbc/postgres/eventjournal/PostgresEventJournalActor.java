b36097882658940349a56f2e1877a2752390fa10
Vaughn Vernon <vaughn@forcomprehension.com>
2018-10-29 21:19:03 -0700

    Implemented AppendResultInterest support.

--- src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/eventjournal/PostgresEventJournalActor.java
+++ src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/eventjournal/PostgresEventJournalActor.java
@@ -1,18 +1,11 @@
-package io.vlingo.symbio.store.state.jdbc.postgres.eventjournal;
+// Copyright © 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
 
-import com.google.gson.Gson;
-import io.vlingo.actors.Actor;
-import io.vlingo.actors.Address;
-import io.vlingo.actors.Definition;
-import io.vlingo.common.Completes;
-import io.vlingo.common.identity.IdentityGenerator;
-import io.vlingo.symbio.Event;
-import io.vlingo.symbio.State;
-import io.vlingo.symbio.store.eventjournal.EventJournal;
-import io.vlingo.symbio.store.eventjournal.EventJournalListener;
-import io.vlingo.symbio.store.eventjournal.EventJournalReader;
-import io.vlingo.symbio.store.eventjournal.EventStreamReader;
-import io.vlingo.symbio.store.state.jdbc.Configuration;
+package io.vlingo.symbio.store.state.jdbc.postgres.eventjournal;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,6 +14,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Consumer;
+
+import com.google.gson.Gson;
+
+import io.vlingo.actors.Actor;
+import io.vlingo.actors.Address;
+import io.vlingo.actors.Definition;
+import io.vlingo.common.Completes;
+import io.vlingo.common.identity.IdentityGenerator;
+import io.vlingo.symbio.Event;
+import io.vlingo.symbio.State;
+import io.vlingo.symbio.store.Result;
+import io.vlingo.symbio.store.eventjournal.EventJournal;
+import io.vlingo.symbio.store.eventjournal.EventJournalListener;
+import io.vlingo.symbio.store.eventjournal.EventJournalReader;
+import io.vlingo.symbio.store.eventjournal.EventStreamReader;
+import io.vlingo.symbio.store.state.jdbc.Configuration;
 
 public class PostgresEventJournalActor extends Actor implements EventJournal<String> {
     private static final String INSERT_EVENT =
@@ -58,39 +68,51 @@ public class PostgresEventJournalActor extends Actor implements EventJournal<Str
     }
 
     @Override
-    public void append(String streamName, int streamVersion, Event<String> event) {
-        insertEvent(streamName, streamVersion, event);
-        doCommit();
+    public void append(final String streamName, final int streamVersion, final Event<String> event, final AppendResultInterest<String> interest, final Object object) {
+        final Consumer<Exception> whenFailed =
+                (e) -> interest.appendResultedIn(Result.Failure, e, streamName, streamVersion, event, object);
+        insertEvent(streamName, streamVersion, event, whenFailed);
+        doCommit(whenFailed);
         listener.appended(event);
+        interest.appendResultedIn(Result.Success, streamName, streamVersion, event, object);
     }
 
     @Override
-    public void appendWith(String streamName, int streamVersion, Event<String> event, State<String> snapshot) {
-        insertEvent(streamName, streamVersion, event);
-        insertSnapshot(streamName, snapshot);
-        doCommit();
+    public void appendWith(final String streamName, final int streamVersion, final Event<String> event, final State<String> snapshot, final AppendResultInterest<String> interest, final Object object) {
+      final Consumer<Exception> whenFailed =
+                (e) -> interest.appendResultedIn(Result.Failure, e, streamName, streamVersion, event, object);
+        insertEvent(streamName, streamVersion, event, whenFailed);
+        insertSnapshot(streamName, snapshot, whenFailed);
+        doCommit(whenFailed);
         listener.appendedWith(event, snapshot);
+        interest.appendResultedIn(Result.Success, streamName, streamVersion, event, snapshot, object);
     }
 
     @Override
-    public void appendAll(String streamName, int fromStreamVersion, List<Event<String>> events) {
+    public void appendAll(final String streamName, final int fromStreamVersion, final List<Event<String>> events, final AppendResultInterest<String> interest, final Object object) {
+      final Consumer<Exception> whenFailed =
+                (e) -> interest.appendResultedIn(Result.Failure, e, streamName, fromStreamVersion, events, object);
         int version = fromStreamVersion;
         for (Event<String> event : events) {
-            insertEvent(streamName, version++, event);
+            insertEvent(streamName, version++, event, whenFailed);
         }
-        doCommit();
+        doCommit(whenFailed);
         listener.appendedAll(events);
+        interest.appendResultedIn(Result.Success, streamName, fromStreamVersion, events, object);
     }
 
     @Override
-    public void appendAllWith(String streamName, int fromStreamVersion, List<Event<String>> events, State<String> snapshot) {
+    public void appendAllWith(final String streamName, final int fromStreamVersion, final List<Event<String>> events, final State<String> snapshot, final AppendResultInterest<String> interest, final Object object) {
+      final Consumer<Exception> whenFailed =
+                (e) -> interest.appendResultedIn(Result.Failure, e, streamName, fromStreamVersion, events, object);
         int version = fromStreamVersion;
         for (Event<String> event : events) {
-            insertEvent(streamName, version++, event);
+            insertEvent(streamName, version++, event, whenFailed);
         }
-        insertSnapshot(streamName, snapshot);
-        doCommit();
+        insertSnapshot(streamName, snapshot, whenFailed);
+        doCommit(whenFailed);
         listener.appendedAllWith(events, snapshot);
+        interest.appendResultedIn(Result.Success, streamName, fromStreamVersion, events, snapshot, object);
     }
 
     @Override
@@ -129,7 +151,11 @@ public class PostgresEventJournalActor extends Actor implements EventJournal<Str
         return completes().with(reader);
     }
 
-    protected final void insertEvent(final String eventStream, final int eventVersion, final Event<String> event) {
+    protected final void insertEvent(
+            final String eventStream,
+            final int eventVersion,
+            final Event<String> event,
+            final Consumer<Exception> whenFailed) {
         try {
             final UUID id = identityGenerator.generate();
             final long timestamp = id.timestamp();
@@ -150,12 +176,16 @@ public class PostgresEventJournalActor extends Actor implements EventJournal<Str
 
             event.__internal__setId(id.toString());
         } catch (SQLException e) {
+            whenFailed.accept(e);
             logger().log("vlingo/symbio-postgres: Could not insert event " + event.toString(), e);
             throw new IllegalStateException(e);
         }
     }
 
-    protected final void insertSnapshot(final String eventStream, final State<String> snapshot) {
+    protected final void insertSnapshot(
+            final String eventStream,
+            final State<String> snapshot,
+            final Consumer<Exception> whenFailed) {
         try {
             insertSnapshot.setString(1, eventStream);
             insertSnapshot.setString(2, snapshot.type);
@@ -169,15 +199,17 @@ public class PostgresEventJournalActor extends Actor implements EventJournal<Str
                 throw new IllegalStateException("vlingo/symbio-postgres: Could not insert event");
             }
         } catch (SQLException e) {
+            whenFailed.accept(e);
             logger().log("vlingo/symbio-postgres: Could not insert event with id " + snapshot.id, e);
             throw new IllegalStateException(e);
         }
     }
 
-    private void doCommit() {
+    private void doCommit(final Consumer<Exception> whenFailed) {
         try {
             connection.commit();
         } catch (SQLException e) {
+            whenFailed.accept(e);
             logger().log("vlingo/symbio-postgres: Could not complete transaction", e);
             throw new IllegalStateException(e);
         }
