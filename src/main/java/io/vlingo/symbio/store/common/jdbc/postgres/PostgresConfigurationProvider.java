23b442d5e8226218a4a644422687256f50a094cc
VaughnVernon <vaughn@forcomprehension.com>
2019-01-26 03:06:12 -0700

    Moved Configuration. Placed DataFormat in own file.

--- src/main/java/io/vlingo/symbio/store/common/jdbc/postgres/PostgresConfigurationProvider.java
+++ src/main/java/io/vlingo/symbio/store/common/jdbc/postgres/PostgresConfigurationProvider.java
@@ -5,15 +5,15 @@
 // was not distributed with this file, You can obtain
 // one at https://mozilla.org/MPL/2.0/.
 
-package io.vlingo.symbio.store.state.jdbc.postgres;
+package io.vlingo.symbio.store.common.jdbc.postgres;
 
 import java.sql.Connection;
 import java.sql.Statement;
 
-import io.vlingo.symbio.store.state.StateStore.DataFormat;
-import io.vlingo.symbio.store.state.jdbc.Configuration;
-import io.vlingo.symbio.store.state.jdbc.Configuration.ConfigurationInterest;
-import io.vlingo.symbio.store.state.jdbc.Configuration.TestConfiguration;
+import io.vlingo.symbio.store.DataFormat;
+import io.vlingo.symbio.store.common.jdbc.Configuration;
+import io.vlingo.symbio.store.common.jdbc.Configuration.ConfigurationInterest;
+import io.vlingo.symbio.store.common.jdbc.Configuration.TestConfiguration;
 
 public class PostgresConfigurationProvider {
   private static final ConfigurationInterest interest = new ConfigurationInterest() {
