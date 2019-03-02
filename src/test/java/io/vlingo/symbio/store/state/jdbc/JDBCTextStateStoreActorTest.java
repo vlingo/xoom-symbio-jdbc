3dde48aa33ae6f1d0b2d6d96a12b2cd62443419f
Dave Muirhead <dave@brsg.biz>
2019-03-01 22:07:26 -0500

    refactored JDBCTextStateStoreActorTest to AccessSafely

--- src/test/java/io/vlingo/symbio/store/state/jdbc/JDBCTextStateStoreActorTest.java
+++ src/test/java/io/vlingo/symbio/store/state/jdbc/JDBCTextStateStoreActorTest.java
@@ -17,8 +17,10 @@ import org.junit.Test;
 
 import io.vlingo.actors.Definition;
 import io.vlingo.actors.World;
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
+import io.vlingo.symbio.State;
 import io.vlingo.symbio.store.DataFormat;
+import io.vlingo.symbio.store.Result;
 import io.vlingo.symbio.store.common.jdbc.Configuration.TestConfiguration;
 import io.vlingo.symbio.store.state.Entity1;
 import io.vlingo.symbio.store.state.Entity1.Entity1StateAdapter;
@@ -39,8 +41,8 @@ public abstract class JDBCTextStateStoreActorTest {
 
   @Test
   public void testThatStateStoreDispatches() throws Exception {
-    interest.until = TestUntil.happenings(6);
-    dispatcher.until = TestUntil.happenings(3);
+    final AccessSafely accessInterest1 = interest.afterCompleting(6);
+    final AccessSafely accessDispatcher1 = dispatcher.afterCompleting(3);
 
     final Entity1 entity1 = new Entity1("123", 1);
     store.write(entity1.id, entity1, 1, interest);
@@ -48,72 +50,86 @@ public abstract class JDBCTextStateStoreActorTest {
     store.write(entity2.id, entity2, 1, interest);
     final Entity1 entity3 = new Entity1("345", 3);
     store.write(entity3.id, entity3, 1, interest);
-
-    dispatcher.until.completes();
-    interest.until.completes();
-
-    assertEquals(3, dispatcher.dispatched.size());
-    assertEquals(3, interest.confirmDispatchedResultedIn.get());
-    assertEquals("123", dispatcher.dispatched.get(dispatchId("123")).id);
-    assertEquals("234", dispatcher.dispatched.get(dispatchId("234")).id);
-    assertEquals("345", dispatcher.dispatched.get(dispatchId("345")).id);
-
-    interest.until = TestUntil.happenings(6);
-    dispatcher.until = TestUntil.happenings(2);
+          
+    assertEquals(3, (int) accessDispatcher1.readFrom("dispatchedStateCount"));
+    assertEquals(3, (int) accessInterest1.readFrom("confirmDispatchedResultedIn"));
+    final State<?> state123 = accessDispatcher1.readFrom("dispatchedState", dispatchId("123"));
+    assertEquals("123", state123.id);
+    final State<?> state234 = accessDispatcher1.readFrom("dispatchedState", dispatchId("234"));
+    assertEquals("234", state234.id);
+    final State<?> state345 = accessDispatcher1.readFrom("dispatchedState", dispatchId("345"));
+    assertEquals("345", state345.id);
     
-    dispatcher.processDispatch.set(false);
+    final AccessSafely accessInterest2 = interest.afterCompleting(4);
+    final AccessSafely accessDispatcher2 = dispatcher.afterCompleting(4);
+
+    accessDispatcher2.writeUsing("processDispatch", false);
     final Entity1 entity4 = new Entity1("456", 4);
     store.write(entity4.id, entity4, 1, interest);
     final Entity1 entity5 = new Entity1("567", 5);
     store.write(entity5.id, entity5, 1, interest);
-    dispatcher.processDispatch.set(true);
+    
+    accessDispatcher2.writeUsing("processDispatch", true);
     dispatcher.control.dispatchUnconfirmed();
+    accessDispatcher2.readFrom("dispatchedStateCount");
 
-    dispatcher.until.completes();
-    interest.until.completes();
-
-    assertEquals(5, dispatcher.dispatched.size());
-    assertEquals(7, interest.confirmDispatchedResultedIn.get());
-    assertEquals("456", dispatcher.dispatched.get(dispatchId("456")).id);
-    assertEquals("567", dispatcher.dispatched.get(dispatchId("567")).id);
+    assertEquals(5, (int) accessDispatcher2.readFrom("dispatchedStateCount"));
+    assertEquals(5, (int) accessInterest2.readFrom("confirmDispatchedResultedIn"));
+    final State<?> state456 = accessDispatcher1.readFrom("dispatchedState", dispatchId("456"));
+    assertEquals("456", state456.id);
+    final State<?> state567 = accessDispatcher1.readFrom("dispatchedState", dispatchId("567"));
+    assertEquals("567", state567.id);
   }
 
   @Test
   public void testThatReadErrorIsReported() {
-    interest.until = TestUntil.happenings(3); // includes write, confirmation, read (not necessarily in that order)
+    final AccessSafely accessInterest1 = interest.afterCompleting(3);
+    dispatcher.afterCompleting(2);
+    
     final Entity1 entity = new Entity1("123", 1);
     store.write(entity.id, entity, 1, interest);
     store.read(null, Entity1.class, interest);
-    interest.until.completes();
-    assertEquals(1, interest.errorCauses.size());
-    assertEquals("The id is null.", interest.errorCauses.poll().getMessage());
-    assertTrue(interest.textReadResult.get().isError());
-    assertNull(interest.stateHolder.get());
     
-    interest.until = TestUntil.happenings(1);
+    assertEquals(1, (int) accessInterest1.readFrom("errorCausesCount"));
+    final Exception cause1 = accessInterest1.readFrom("errorCauses");
+    assertEquals("The id is null.", cause1.getMessage());
+    Result result1 = accessInterest1.readFrom("textReadResult");
+    assertTrue(result1.isError());
+    assertNull(accessInterest1.readFrom("stateHolder"));
+    
+    interest = new MockResultInterest();
+    final AccessSafely accessInterest2 = interest.afterCompleting(1);
+    final AccessSafely accessDispatcher2 = dispatcher.afterCompleting(1);
+    
     store.read(entity.id, null, interest);  // includes read
-    interest.until.completes();
-    assertEquals(1, interest.errorCauses.size());
-    assertEquals("The type is null.", interest.errorCauses.poll().getMessage());
-    assertTrue(interest.textReadResult.get().isError());
-    assertNull(interest.stateHolder.get());
+    
+    assertEquals(1, (int) accessInterest2.readFrom("errorCausesCount"));
+    final Exception cause2 = accessInterest2.readFrom("errorCauses");
+    assertEquals("The type is null.", cause2.getMessage());
+    Result result2 = accessInterest2.readFrom("textReadResult");
+    assertTrue(result2.isError());
+    final Object objectState = accessInterest2.readFrom("stateHolder");
+    assertNull(objectState);
   }
 
   @Test
   public void testThatWriteErrorIsReported() {
-    interest.until = TestUntil.happenings(1);
-    store.write(null, null, 0, interest);
-    interest.until.completes();
-    assertEquals(1, interest.errorCauses.size());
-    assertEquals("The state is null.", interest.errorCauses.poll().getMessage());
-    assertTrue(interest.textWriteAccumulatedResults.poll().isError());
-    assertNull(interest.stateHolder.get());
-  }
+    final AccessSafely accessInterest1 = interest.afterCompleting(1);
+    dispatcher.afterCompleting(1);
 
+    store.write(null, null, 0, interest);
+    
+    assertEquals(1, (int) accessInterest1.readFrom("errorCausesCount"));
+    final Exception cause1 = accessInterest1.readFrom("errorCauses");
+    assertEquals("The state is null.", cause1.getMessage());
+    final Result result1 = accessInterest1.readFrom("textWriteAccumulatedResults");
+    assertTrue(result1.isError());
+    final Object objectState = accessInterest1.readFrom("stateHolder");
+    assertNull(objectState);
+  }
+  
   @Before
   public void setUp() throws Exception {
-    System.out.println("Starting: " + getClass().getSimpleName() + ": setUp()");
-
     world = World.startWithDefaults("test-store");
 
     entity1StoreName = Entity1.class.getSimpleName();
@@ -123,7 +139,7 @@ public abstract class JDBCTextStateStoreActorTest {
 
     delegate = delegate();
 
-    interest = new MockResultInterest(0);
+    interest = new MockResultInterest();
     dispatcher = new MockTextDispatcher(0, interest);
 
     store = world.actorFor(
@@ -134,12 +150,10 @@ public abstract class JDBCTextStateStoreActorTest {
 
   @After
   public void tearDown() throws Exception {
-    System.out.println("Ending: " + getClass().getSimpleName() + ": tearDown()");
-
     if (configuration == null) return;
+    world.terminate();
     configuration.cleanUp();
     delegate.close();
-    world.terminate();
   }
 
   protected abstract StorageDelegate delegate() throws Exception;
