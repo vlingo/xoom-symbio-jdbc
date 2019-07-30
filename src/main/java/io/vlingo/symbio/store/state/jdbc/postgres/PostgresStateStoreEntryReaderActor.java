a32da6d479d63444c056cb118ff5f778d6491499
VaughnVernon <vaughn@forcomprehension.com>
2019-07-29 22:16:11 -0700

    Cannot use default implementation with actors.

--- src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/PostgresStateStoreEntryReaderActor.java
+++ src/main/java/io/vlingo/symbio/store/state/jdbc/postgres/PostgresStateStoreEntryReaderActor.java
@@ -71,9 +71,21 @@ public class PostgresStateStoreEntryReaderActor<T extends Entry<?>> extends Acto
   }
 
   @Override
+  public Completes<T> readNext(final String fromId) {
+    seekTo(fromId);
+    return readNext();
+  }
+
+  @Override
   @SuppressWarnings("unchecked")
   public Completes<List<T>> readNext(final int maximumEntries) {
     return completes().with((List<T>) queryNext(maximumEntries));
+  }
+
+  @Override
+  public Completes<List<T>> readNext(final String fromId, final int maximumEntries) {
+    seekTo(fromId);
+    return readNext(maximumEntries);
   }
 
   @Override
