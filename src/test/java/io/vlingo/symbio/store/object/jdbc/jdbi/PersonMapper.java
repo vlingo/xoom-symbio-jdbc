6a33023d4367ac4ebb40108253330bdd8c53150c
VaughnVernon <vaughn@forcomprehension.com>
2019-08-14 23:42:18 -0700

    Removed Person import.

--- src/test/java/io/vlingo/symbio/store/object/jdbc/jdbi/PersonMapper.java
+++ src/test/java/io/vlingo/symbio/store/object/jdbc/jdbi/PersonMapper.java
@@ -13,8 +13,6 @@ import java.sql.SQLException;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
-import io.vlingo.symbio.store.object.jdbc.Person;
-
 public class PersonMapper implements RowMapper<Person> {
   @Override
   public Person map(final ResultSet rs, final StatementContext ctx) throws SQLException {
