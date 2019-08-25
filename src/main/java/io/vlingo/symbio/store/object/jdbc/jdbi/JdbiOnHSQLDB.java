ea756e2dc11c224f5f5007919452386c7af12dda
VaughnVernon <vaughn@forcomprehension.com>
2019-08-25 00:10:15 -0700

    Cast to silence warning.

--- src/main/java/io/vlingo/symbio/store/object/jdbc/jdbi/JdbiOnHSQLDB.java
+++ src/main/java/io/vlingo/symbio/store/object/jdbc/jdbi/JdbiOnHSQLDB.java
@@ -8,6 +8,9 @@
 package io.vlingo.symbio.store.object.jdbc.jdbi;
 
 import java.text.MessageFormat;
+import java.util.function.BiFunction;
+
+import org.jdbi.v3.core.statement.Update;
 
 import io.vlingo.symbio.store.common.jdbc.Configuration;
 import io.vlingo.symbio.store.object.jdbc.JDBCObjectStoreEntryJournalQueries;
@@ -46,7 +49,7 @@ public class JdbiOnHSQLDB extends JdbiOnDatabase {
                     placeholders[0],
                     placeholders[1]),
             null,
-            null);
+            (BiFunction<Update,Object,Update>[]) null);
   }
 
   private JdbiOnHSQLDB(final Configuration configuration) {
