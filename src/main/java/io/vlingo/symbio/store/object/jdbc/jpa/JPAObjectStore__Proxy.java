7e0632b96c03e12d0e417e3c368da07a523b273e
VaughnVernon <vaughn@forcomprehension.com>
2019-08-30 08:12:44 -0500

    Fixed preallocated Mailbox send.

--- src/main/java/io/vlingo/symbio/store/object/jdbc/jpa/JPAObjectStore__Proxy.java
+++ src/main/java/io/vlingo/symbio/store/object/jdbc/jpa/JPAObjectStore__Proxy.java
@@ -79,7 +79,7 @@ public class JPAObjectStore__Proxy implements io.vlingo.symbio.store.object.jdbc
     if (!actor.isStopped()) {
       final java.util.function.Consumer<ObjectStore> consumer = (actor) -> actor.entryReader(arg0);
       final Completes<EntryReader<? extends Entry<?>>> completes = new BasicCompletes<>(actor.scheduler());
-      if (mailbox.isPreallocated()) { mailbox.send(actor, ObjectStore.class, consumer, completes, entryReaderRepresentation4); }
+      if (mailbox.isPreallocated()) { mailbox.send(actor, ObjectStore.class, consumer, Returns.value(completes), entryReaderRepresentation4); }
       else { mailbox.send(new LocalMessage<ObjectStore>(actor, ObjectStore.class, consumer, Returns.value(completes), entryReaderRepresentation4)); }
       return completes;
     } else {
