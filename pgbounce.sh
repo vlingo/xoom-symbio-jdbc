5b190716029f90e1f5fec7d1ac1e75eb345fbce7
VaughnVernon <vaughn@forcomprehension.com>
2019-09-26 03:57:39 -0700

    Run mysql script inside container.

--- pgbounce.sh
+++ pgbounce.sh
@@ -1,4 +1,11 @@
-# bounce the docker volume vlingo-symbio-jdbc-postgres in the docker-compose.yml
+# bounce the docker volume vlingo-symbio-jdbc-postgres and vlingo-symbio-jdbc-mysql in the docker-compose.yml
 docker-compose -p "dev" down
 docker volume rm vlingo-symbio-jdbc-postgres
+docker volume rm vlingo-symbio-jdbc-mysql
 docker-compose -p "dev" up -d
+
+cwd=$(pwd)
+echo ${cwd}
+mysqlcmd=("'mysql -u root -p vlingo123 -e "${cwd}"/mysql_init.sql'")
+echo "${mysqlcmd[@]}"
+docker exec -it vlingo_mysql "${mysqlcmd[@]}"
