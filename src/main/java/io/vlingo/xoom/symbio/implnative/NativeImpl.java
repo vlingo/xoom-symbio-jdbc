694820f84b0558e10f9f5ac78de1bb4534f09ba6
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-11 07:58:50 +0100

    Adding GraalVM Dashboard Support..

---
+++ src/main/java/io/vlingo/xoom/symbio/implnative/NativeImpl.java
@@ -0,0 +1,21 @@
+package io.vlingo.xoom.symbio.implnative;
+
+import io.vlingo.xoom.actors.World;
+import io.vlingo.xoom.symbio.EntryAdapterProvider;
+import io.vlingo.xoom.symbio.StateAdapterProvider;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+
+public final class NativeImpl {
+  @CEntryPoint(name = "Java_io_vlingo_xoom_symbio_jdbcnative_Native_start")
+  public static int start(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer name) {
+    final String nameString = CTypeConversion.toJavaString(name);
+    World world = World.startWithDefaults(nameString);
+
+    final StateAdapterProvider stateAdapterProvider = new StateAdapterProvider(world);
+    new EntryAdapterProvider(world);
+    return 0;
+  }
+}
+
